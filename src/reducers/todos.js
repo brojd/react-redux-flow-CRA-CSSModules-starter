f4978132c6b14442e5e05e3459d9e129d2534a70
Dominik Broj <dominik.broj@dataart.com>
2017-02-28 12:21:30 +0100

    use flow in reducers

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,3 +1,9 @@
+/* @flow */
+
+import type { Todo } from '../components/TodoList/TodoList.component';
+
+type State = Array<Todo>
+
 const initialState = [
   { id: 0, name: '1st task', completed: false },
   { id: 1, name: '2nd task', completed: false }
@@ -5,7 +11,7 @@ const initialState = [
 
 let nextId = 2;
 
-const todos = (state = initialState, action) => {
+const todos = (state: State = initialState, action: Object): State => {
   switch (action.type) {
     case 'ADD_TODO':
       return [ ...state, Object.assign({ ...action.newTodo, id: nextId++ }) ];
--- src/reducers/todosFilter.js
+++ src/reducers/todosFilter.js
@@ -1,6 +1,8 @@
+/* @flow */
+
 const initialState = 'ACTIVE';
 
-const counter = (state = initialState, action) => {
+const counter = (state: string = initialState, action: Object): string => {
   switch (action.type) {
     case 'SET_FILTER':
       return action.filter;
